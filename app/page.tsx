9283c76f74673fd5b5d8d74c82820e484c2ded66
UMIKAZE <c463384962@gmail.com>
2026-06-15 15:44:23 +0900

    Redirect home to notices

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-white px-5">
-      <Link
-        href="/notices"
-        className="rounded-md bg-cyan-500 px-5 py-3 text-sm font-black text-white"
-      >
-        NEWS
-      </Link>
-    </main>
-  );
+  redirect("/notices");
 }
